dd8556425e99910ff3f4e72a8d524716de4298fd
DorianSGT <santigarcia15087@gmail.com>
2026-01-27 19:52:47 +0100

    mejora rendimiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   output: 'static',
   build: {
     assets: '_assets'
